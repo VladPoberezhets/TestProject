b3345c9e49c7ba9000762a92dd046b1d0a1b2535
Vladyslav Poberezhets <v.poberezhets.pnn.in.ua>
2025-01-09 19:07:12 +0200

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Vlad Poberezhets on 09.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
